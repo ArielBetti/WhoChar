56c1710e45b4ca455e48fdf9f6c570fac1a3d6d8
ArielBetti <ariel.betti@gmail.com>
2019-12-26 23:02:35 -0300

    Corrigindo diretorio

--- src/pages/Login/index.js
+++ src/pages/Login/index.js
@@ -15,9 +15,12 @@ export default function () {
                     <div className="buttonslogin">
                         <button className="signin">Sign in</button>
                         <button className="signup">Sign up</button>
+                        <div class="fb-login-button" data-width="" data-size="large" data-button-type="continue_with" data-auto-logout-link="false" data-use-continue-as="true"></div>
                     </div>
                 </div>
             </div>
+            <div id="fb-root"></div>
+<script async defer crossorigin="anonymous" src="https://connect.facebook.net/pt_BR/sdk.js#xfbml=1&version=v5.0&appId=576678136423139&autoLogAppEvents=1"></script>
         </div>
     );
 }
